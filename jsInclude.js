178b9707b52c497e07bee6474f954349aeca16a9
Michael Trotter <spicydonuts@gmail.com>
2015-02-07 13:09:55 -0700

    no leaky scope; code reuse; async loading

--- jsInclude.js
+++ jsInclude.js
@@ -1,35 +1,31 @@
-var JsInclude = {
-    header: function(files, callback) {
-        window.onload = function(){
-            var head = document.getElementsByTagName('head')[0];
+var JsInclude = (function(document) {
+  var callbacks = 0;
 
-            files.files.forEach(function(val) {
-                var script= document.createElement('script');
-                script.type= 'text/javascript';
-                script.src = files.folder + "/" + val;
-                head.appendChild(script);
-            });
-
-            if (callback) {
-                callback();
-            }
-        };
+  return {
+    header: function(bundle, callback) {
+      var head = document.getElementsByTagName('head')[0];
+      appendTo(head, bundle, callback);
     },
 
-    footer: function(files, callback) {
-        window.onload = function(){
-            var body = document.getElementsByTagName('body')[0];
+    footer: function(bundle, callback) {
+      var body = document.getElementsByTagName('body')[0];
+      appendTo(body, bundle, callback);
+    },
 
-            files.files.forEach(function(val) {
-                var script= document.createElement('script');
-                script.type= 'text/javascript';
-                script.src = files.folder + "/" + val;
-                body.appendChild(script);
-            });
-
-            if (callback) {
-                callback();
-            }
-        };
+    appendTo(element, bundle, callback) {
+      window.onload = function(){
+        bundle.files.forEach(function(val) {
+          var script = document.createElement('script');
+          script.type = 'text/javascript';
+          script.async = true;
+          callbacks++;
+          script.onload = function() {
+            if (--callbacks === 0) callback();
+          };
+          script.src = files.folder + "/" + val;
+          body.appendChild(script);
+        });
+      };
     }
-};
+  };
+})(document);
